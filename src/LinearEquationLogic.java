57cbe6935f45756cd74f5b4665a35460c2b20ad7
BT_1N3_01 <sample@bths.edu>
2023-10-17 11:01:54 -0400

    oct 17

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -2,7 +2,43 @@ import java.util.Scanner;
 
 public class LinearEquationLogic {
 
-    private Scanner myscanner;
+    private Scanner myScanner;
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+
+    public void LinearEquation () {
+        myScanner = new Scanner(System.in);
+
+    }
+    private void enterOrderedPair() {
+        String orderedPair = "";
+        String orderedPair2 = "";
+        System.out.print("Hello, enter an ordered pair(in this format (x, y): ");
+        orderedPair = myScanner.nextLine();
+
+        int index = orderedPair.indexOf(",");
+        String digit1 = orderedPair.substring(1, index);
+        int x1 = Integer.parseInt(digit1);
+
+        String digit2 = orderedPair.substring(index + 2, orderedPair.length() - 1);
+        int y1 = Integer.parseInt(digit2);
+
+        System.out.print("Hello, enter another ordered pair(in this format (x, y): ");
+        orderedPair2 = myScanner.nextLine();
+
+        String digit3 = orderedPair2.substring(1, index);
+        int x2 = Integer.parseInt(digit3);
+
+        String digit4 = orderedPair2.substring(index + 2, orderedPair2.length() - 1);
+        int y2 = Integer.parseInt(digit4);
+    }
+    private void start() {
+        enterOrderedPair();
+
+    }
 
 
 }
