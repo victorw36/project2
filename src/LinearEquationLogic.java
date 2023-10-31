cfdee35f721a8c5675c34f8e4be505e8612afcd9
victorw36 <victorw36@nycstudents.net>
2023-10-30 23:56:07 -0400

    90 percent done

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -3,20 +3,17 @@ import java.util.Scanner;
 public class LinearEquationLogic {
 
     private Scanner myScanner;
-    private int x1;
-    private int x2;
-    private int y1;
-    private int y2;
+    private LinearEquation linearEquation;
 
 
-    public void LinearEquation () {
+    public LinearEquationLogic() {
         myScanner = new Scanner(System.in);
-
+        linearEquation = null;
     }
     private void enterOrderedPair() {
         String orderedPair = "";
         String orderedPair2 = "";
-        System.out.print("Hello, enter an ordered pair(in this format (x, y): ");
+        System.out.print("Hello, enter an ordered pair(in this format (x, y)): ");
         orderedPair = myScanner.nextLine();
 
         int index = orderedPair.indexOf(",");
@@ -26,17 +23,42 @@ public class LinearEquationLogic {
         String digit2 = orderedPair.substring(index + 2, orderedPair.length() - 1);
         int y1 = Integer.parseInt(digit2);
 
-        System.out.print("Hello, enter another ordered pair(in this format (x, y): ");
+        System.out.print("Hello, enter another ordered pair(in this format (x, y))(Don't enter the same x value): ");
         orderedPair2 = myScanner.nextLine();
 
+        index = orderedPair2.indexOf(",");
         String digit3 = orderedPair2.substring(1, index);
         int x2 = Integer.parseInt(digit3);
 
         String digit4 = orderedPair2.substring(index + 2, orderedPair2.length() - 1);
         int y2 = Integer.parseInt(digit4);
+
+        linearEquation = new LinearEquation(x1, y1, x2, y2);
+
     }
-    private void start() {
+    public void start() {
         enterOrderedPair();
+        System.out.print(linearEquation.lineInfo());
+        System.out.println();
+        System.out.print("Enter a value for x: ");
+        double x = myScanner.nextDouble();
+        System.out.println(linearEquation.coordinateForX(x));
+        myScanner.nextLine();
+
+        System.out.print("Do you want to enter more coordinates(y/n): ");
+        String answer = myScanner.nextLine();
+        while (answer.equals("y")) {
+            enterOrderedPair();
+            System.out.print(linearEquation.lineInfo());
+            System.out.println();
+            System.out.print("Enter a value for x: ");
+            x = myScanner.nextInt();
+            System.out.println(linearEquation.coordinateForX(x));
+            System.out.print("Do you want to enter more coordinates(y/n): ");
+            answer = myScanner.nextLine();
+        }
+
+
 
     }
 
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,5 +1,13 @@
 public class LinearEquationRunner {
     public static void main(String[] args) {
+        LinearEquationLogic logic = new LinearEquationLogic();
+        logic.start();
+
+
+
+
+
+
 
 
 
